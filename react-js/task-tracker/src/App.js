567fd71910eccc456dcc48aa3a027df4bb7662d7
Mohd-Afzal <mohammedafsal878@gmail.com>
2021-12-09 22:52:40 +0530

    First React JS Application

---
+++ react-js/task-tracker/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1> This is the first start up page for React JS </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-js/task-tracker/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    {/* Component - 1 */}
+    {/* Component - 2 */} 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
