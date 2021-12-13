62f16bdbdf1855c4ba034471d583b4908efb5658
Mohd-Afzal <mohammedafsal878@gmail.com>
2021-12-13 22:27:39 +0530

    Closures in JS - 1

---
+++ JavaScript/index1.js
@@ -0,0 +1,20 @@
+console.log("JavaScript is running from Index1.js!");
+
+var b = function() {
+    var fileName = "index1.js";
+    console.log(fileName);
+} ();
+
+
+// var num = 10; // Declared and Initialized 
+// var num = 20; // num is defined already - It basically reassigns num with 20 
+
+// console.log(num);
+
+
+
+
+
+
+
+
---
+++ JavaScript/index2.js
@@ -0,0 +1,6 @@
+console.log("JavaScript is running from Index2.js!");
+
+var b = function () {
+    var fileName = "index2.js";
+    console.log(fileName);
+} ();
