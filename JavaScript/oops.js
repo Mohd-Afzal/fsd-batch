e462420a4310b121451e8e229ef33998571dae48
Mohd-Afzal <mohammedafsal878@gmail.com>
2021-12-17 20:40:32 +0530

    Covered Constructors and Prototypes

--- JavaScript/oops.js
+++ JavaScript/oops.js
@@ -134,10 +134,15 @@ function Human(hairColor, gender, name) {
     this.name = name;
         
     // Define methods or functions
-    this.eat = function() {
-        console.log("I eat food!");
-    }
 
+    // this.eat = function(a, b) {
+    //     console.log(this.hairColor);
+    //     let sum = a + b;
+    //     console.log("Sum is: ", sum)
+    //     console.log("I eat food!");
+    // }
+
+    // Not defined in the prototype
     this.getCurrentReference = function () {
         console.log(this);
     }
@@ -146,14 +151,67 @@ function Human(hairColor, gender, name) {
 }
 
 
+// var afzal = new Human("black", "male", "Afzal");
+// var monica = new Human("black", "female", "Monica");
+// var israel = new Human("blonde", "male", "Isarel");
+// var swammy = new Human("black", "male", "Swammy");
+
+
+// afzal.getCurrentReference();
+// monica.getCurrentReference();
+// israel.getCurrentReference();
+// swammy.getCurrentReference();
+
+
+
+
+
+/* -------- Prototypes -------- */
+
+// Moving the eat function to the prototype of Human
+Human.prototype.eat = function(a, b) {
+    console.log(this.hairColor);
+    let sum = a + b;
+    console.log("Sum is: ", sum)
+    console.log("I eat food!");
+}
+
 var afzal = new Human("black", "male", "Afzal");
-var monica = new Human("black", "female", "Monica");
-var israel = new Human("blonde", "male", "Isarel");
-var swammy = new Human("black", "male", "Swammy");
+// afzal.eat(30, 50); // Calling the eat function using the object which is defined in the prototype actually
 
 
-afzal.getCurrentReference();
-monica.getCurrentReference();
-israel.getCurrentReference();
-swammy.getCurrentReference();
+// Defining properties in the protoype
+Human.prototype.numberOfLegs = 2;
+Human.prototype.numberOfHands = 2;
 
+
+
+// Creating an EMpty Object
+const obj1 = {};
+const obj2 = new Object();
+
+
+// Discuss about the methods
+
+const objProto = Object.prototype;
+const humanProto = Human.prototype
+
+if (humanProto === objProto) {
+    console.log("TRUE");
+} else {
+    console.log("False");
+}
+
+// Alternate to writing the above code
+// const isTrue = Object.prototype.isPrototypeOf(Human.prototype);
+// console.log(isTrue);
+
+/* 
+    1. Hirearchy of the objects defined in JavaScript
+    2. object VS Object
+    3. Classes
+    4. Inheritance
+*/
+
+
+/*  End  */
