c0d0e3cbfcfc36d9018a618c70073782b392094d
Mohd-Afzal <mohammedafsal878@gmail.com>
2022-01-20 20:28:39 +0530

    Creating cutom Hook, react-router-dom@5(intro)

--- react-js/task-tracker/src/App.js
+++ react-js/task-tracker/src/App.js
@@ -2,7 +2,7 @@ import Button from "./components/Button";
 import PersonDetail from "./components/PersonDetail";
 import './App.css';
 import { useState, useEffect } from 'react';
-
+import useFetch from "./useFetch";
 
 const containerLayout = {
   display: "flex",
@@ -10,37 +10,44 @@ const containerLayout = {
 }
 
 function App() {
-  const [persons, setPersons] = useState(null);
   const [showData, setShowData] = useState(false);
   const [buttonText, setButtonText] = useState("Show Data");
-  const [demoName, setDemoName] = useState("Aakanksha");
 
-  useEffect(() => {
-    console.log("Use Effect Hook's callback has been invoked!");
-    // Fetch the data from a server - JSON Server
+  const {data: persons, triggerFetch} = useFetch('http://localhost:8000/persons');
 
-    // returns a promise
-    setTimeout(() => {
-      const resource = "persons";
-      fetch(`http://localhost:8000/${resource}`)
-      .then((response) => {
-        console.log("Fist Promise: ", response);
-        if(!response.ok) {
-          throw Error(`Unable to reach the mentioned resource -${resource}`);
-        }
-        return response.json();
-      })
-      .then((response) => {
-        console.log("Second Promise", response);
-        setPersons(response);
-        setShowData(true);
-      }).catch((error) => {
-        console.log(error.message);
-      });
-    }, 4000);
+  // const fetchData = () => {
+  //   console.log("Use Effect Hook's callback has been invoked!");
+  //   // Fetch the data from a server - JSON Server
+  //   // returns a promise
+  //   setTimeout(() => {
+  //     const resource = "persons";
+  //     fetch(`http://localhost:8000/${resource}`)
+  //     .then((response) => {
+  //       console.log("Fist Promise: ", response);
+  //       if(!response.ok) {
+  //         throw Error(`Unable to reach the mentioned resource -${resource}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((response) => {
+  //       console.log("Second Promise", response);
+  //       setPersons(response);
+  //       setTriggerFetch(true);
+  //       // setShowData(true);
+  //     }).catch((error) => {
+  //       console.log(error.message);
+  //     });
+  //   }, 4000);
+  // }
 
-  }, []);
-
+  // useEffect(() => {
+  //   if (showData) { 
+  //     console.log("Use Effect Triggered on Button Click");
+  //     fetchData();
+  //   } else {
+  //     console.log("UseEffect Called for the first time when Application reloaded!");
+  //   }
+  // }, [showData]);
 
   function togglePersons() {
     if (showData) {
@@ -53,20 +60,22 @@ function App() {
   }
 
   const deletePerson = (id) => {
-    console.log(id);
-    // Delete a perticalar id from the persons!
-    // To splice! -> higher order array method
+    // console.log(id);
+    // // Delete a perticalar id from the persons!
+    // // To splice! -> higher order array method
     
-    // Create a duplicate array out of the state variable -> coz state variables are immutable
-    const duplciatePersons = [...persons]; // spread operator or Object.assign()
-    // duplciatePersons.splice(id, 1); // We cant use index as the key to delete as it destroys the robustness and cohesiveness of the data/state
+    // // Create a duplicate array out of the state variable -> coz state variables are immutable
+    // const duplciatePersons = [...persons]; // spread operator or Object.assign()
+    // // duplciatePersons.splice(id, 1); // We cant use index as the key to delete as it destroys the robustness and cohesiveness of the data/state
     
-    setPersons(duplciatePersons.filter(person => person.id !== id));
-    console.log("Entry Succeefully deleted in App.js's state!");
+    // setPersons(duplciatePersons.filter(person => person.id !== id));
+    // console.log("Entry Succeefully deleted in App.js's state!");
+
+    console.log("Delete Called");
   }
 
   var renderPerson = null;
-  if (showData) {
+  if (triggerFetch && showData) {
     renderPerson = (
       /* Map to iterate over the list of persons */
       persons.map((person) => {
@@ -77,23 +86,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1 id = "heading"> Task Tracker Application </h1>
-      <div style={containerLayout}>
-        {/* <Button click = {togglePersons} text = {buttonText}/> */}
-      
-        {
-          !showData && <h1> Loading Persons Data .... </h1>
-        }
-        {
-          // renderPerson
-          showData && persons.map((person) => {
-            return <PersonDetail key = {person.id} id = {person.id} person = {person} deletePerson = {deletePerson}/>
-          })
-        }
+      <h1 id = "heading"> Task Tracker Application </h1> 
+      <div style={containerLayout}> 
+        <Button click = {togglePersons} text = {buttonText}/>
+        {!triggerFetch && <h1> Loading Persons Data .... </h1>}
+        {renderPerson}
       </div>
-
-      {/* <h1> {demoName} </h1>
-      <button className="btn btn-danger" onClick={() => {setDemoName("John Doe")}}> Change Name </button> */}
     </div>
   );
 }
